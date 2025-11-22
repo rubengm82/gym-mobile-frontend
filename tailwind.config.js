9dcb56e5440fc969521ae0cbecea55e40799fe54
ruben <ruben.gallardo@inslapineda.cat>
2025-11-22 01:46:05 +0100

    Configuracion de Tailwind(nativewind), esLint y Prettier Local.

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './App.{js,ts,tsx}',
+    './components/**/*.{js,ts,tsx}',
+    './app/**/*.{js,ts,tsx}',
+  ],
+
+  presets: [require('nativewind/preset')],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
